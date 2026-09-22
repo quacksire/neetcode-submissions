beb6763b221b00df13e81c7dacee1d16d2ed5051
Sam Jeffs <sam@quacksire.dev>
2026-09-22 10:48:54 -0700

    Add: maximum-subarray - submission-2

---
+++ Data Structures & Algorithms/maximum-subarray/submission-2.ts
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    maxSubArray(nums: number[]): number {
+        // []
+        // [ , ]
+        // [ , , ]
+        // [ , , , ,]
+        // [ , , , , ...]
+
+        let current = nums[0];
+
+        let best = nums[0];
+
+        for (let i = 1; i < nums.length; i++) {
+
+            current = Math.max(nums[i], current + nums[i]);
+
+            best = Math.max(best, current);
+
+        }
+
+        return best;
+
+    }
+}
